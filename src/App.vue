224cffa8e7f61c86ad99f36cd3f239defdf028c9
xiplus <huangxuanyuxiplus@gmail.com>
2021-11-17 00:18:13 +0800

    Use watchPosition to get location updates

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <h2>陽明交大光復校區實聯制常用連結 v{{ appVersion }}</h2>
+  <h2>陽明交大光復校區實聯制常用連結 <a href="https://github.com/Xi-Plus/contact-based-policy-list#Updates">v{{ appVersion }}</a></h2>
   <div>
     如有錯誤、缺漏、其他建議請在<a
 href="https://github.com/Xi-Plus/contact-based-policy-list/issues"
@@ -113,7 +113,11 @@ export default {
           maximumAge: 0,
           timeout: 30000,
         }
-        navigator.geolocation.getCurrentPosition(success, error, options)
+        var geoId = navigator.geolocation.watchPosition(success, error, options)
+
+        setTimeout(() => {
+          navigator.geolocation.clearWatch(geoId)
+        }, 30000)
       }
     },
   },
